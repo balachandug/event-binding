e15ba79530e55ba0c63cf57eed4ec9eed1e583f3
Balu <balachandu.gillella@sap.com>
2020-05-03 17:32:00 +0530

    test

--- src/App.js
+++ src/App.js
@@ -2,25 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.handleClick2 = this.handleClick1.bind(this);
+  }
+
+  handleClick1() {
+    console.log("button 1 clicked");
+  }
+
+  handleClick3 = () => console.log("button 3 clicked");
+
+  render() {
+    return (
+        <div>
+          <button onClick={this.handleClick1()}>Click 1 </button>
+          <button onClick={this.handleClick1}>Click 2 </button>
+          <button onClick={this.handleClick2}>Click 3 </button>
+          <button onClick={this.handleClick3}>Click 4 </button>
+        </div>
+  )
+  }
 }
+
 
 export default App;
